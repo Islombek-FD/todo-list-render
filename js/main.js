e2b561882094fd44a7e7f3b160fc271b9dd1c951
Islombek-FD <islombekqurbonovfd@gmail.com>
2021-10-13 16:08:41 +0300

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,100 +1,116 @@
-const elTodoForm = document.querySelector('.todo-form');
-const elTodoInput = document.querySelector('.todo-input');
+const elForm = document.querySelector('.todo-form');
+const elInput = document.querySelector('.todo-input');
+const elBtnGroup = document.querySelector('.btn-group');
 const elTodoList = document.querySelector('.todo-list');
-const elTodoAllBtn = document.querySelector('.todo-all__btn');
-const elTodoCompBtn = document.querySelector('.todo-completed__btn');
-const elTodoUnCompBtn = document.querySelector('.todo-uncompleted__btn');
 
-let todos = [];
+const elAllBtn = document.querySelector('.todo-all__btn');
+const elCompletedBtn = document.querySelector('.todo-completed__btn');
+const elUncompletedBtn = document.querySelector('.todo-uncompleted__btn');
 
-elTodoList.addEventListener('click', evt => {
-    if (evt.target.matches('.todo-delete__btn')) {
+const elAllBadge = document.querySelector('.todo-all__badge');
+const elCompletedBadge = document.querySelector('.todo-completed__badge');
+const elUncompletedBadge = document.querySelector('.todo-uncompleted__badge');
+
+const localStorageTodos = JSON.parse(window.localStorage.getItem('todos'));
+
+const todos = localStorageTodos || [];
+
+function renderSaveTodos() {
+	renderTodos(todos, elTodoList);
+	window.localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+renderSaveTodos();
+
+elTodoList.addEventListener('click', (evt) => {
+
+    if (evt.target.matches('.todo-delete-btn')) {
         const todoId = Number(evt.target.dataset.todoId);
         const foundTodoIndex = todos.findIndex(todo => todo.id === todoId);
         todos.splice(foundTodoIndex, 1);
-        renderTodos(todos, elTodoList);
+
+        renderSaveTodos();
     }
     else if (evt.target.matches('.todo-checkbox')) {
         const todoId = Number(evt.target.dataset.todoId);
         const foundTodo = todos.find(todo => todo.id === todoId);
         foundTodo.isCompleted = !foundTodo.isCompleted;
-        renderTodos(todos, elTodoList);
+
+        renderSaveTodos();
     }
 })
-const elCostumLabel = document.querySelector('.todo-label');
 
 function renderTodos(arr, node) {
     node.innerHTML = null;
-    arr.forEach( todo => {
-        const elTodoItem = document.createElement('li');
-        const elTodoBtn = document.createElement('button');
-        const elTodoLabel = document.createElement('label');
-        const elTodoCheckbox = document.createElement('input');
 
-        elTodoItem.textContent = todo.title;
-        elTodoBtn.textContent = 'Delete';
-        
-        elTodoItem.setAttribute('class', 'todo-item');
-        elTodoCheckbox.type = 'checkbox';
-        elTodoLabel.setAttribute('class', 'todo-label');
-        elTodoLabel.setAttribute('for', `todo-checkbox-${todo.id}`);
-        elTodoBtn.classList.add('todo-delete__btn');
-        elTodoCheckbox.classList.add('todo-checkbox');
-        elTodoCheckbox.setAttribute('id', `todo-checkbox-${todo.id}`);
-        elTodoBtn.dataset.todoId = todo.id;
-        elTodoCheckbox.dataset.todoId = todo.id;
+    elAllBadge.textContent = todos.length;
+    elCompletedBadge.textContent = todos.filter(todo => todo.isCompleted === true).length;
+    elUncompletedBadge.textContent = todos.filter(todo => todo.isCompleted === false).length;
+
+    arr.forEach(todo => {
+        const todoLi = document.createElement('li');
+        const todoButton = document.createElement('button');
+        const todoLabel = document.createElement('label');
+        const todoCheckbox = document.createElement('input');
+
+        todoLi.textContent = todo.title;
+        todoButton.textContent = 'Delete';
+
+        todoLi.classList.add('todo-item');
+        todoButton.classList.add('todo-delete-btn');
+        todoLabel.classList.add('todo-label');
+        todoCheckbox.classList.add('todo-checkbox');
+        todoCheckbox.type = 'checkbox';
+        todoLabel.dataset.todoId = todo.id;
+        todoButton.dataset.todoId = todo.id;
+        todoCheckbox.dataset.todoId = todo.id;
 
         if (todo.isCompleted) {
-            elTodoCheckbox.checked = true;
-            elTodoItem.style.textDecoration = 'line-through';
+            todoCheckbox.checked = true;
+            todoLabel.classList.add('todo-label--checked');
+            todoLi.style.color = 'green';
         }
-        elTodoLabel.appendChild(elTodoCheckbox);
-        elTodoItem.appendChild(elTodoLabel);
-        elTodoItem.appendChild(elTodoBtn)
-        node.appendChild(elTodoItem);
-    });
+
+        todoLabel.appendChild(todoCheckbox);
+
+        todoLi.appendChild(todoLabel);
+        todoLi.appendChild(todoButton);
+
+        node.appendChild(todoLi);
+    })
 }
 
-let elBadgeBtn = document.createElement('strong');
-elBadgeBtn.classList.add('todo-badge__btn');
-
-elTodoForm.addEventListener('submit', evt => {
+elForm.addEventListener('submit', (evt) => {
     evt.preventDefault();
-    
-    elBadgeBtn.textContent = todos.length + 1;
-    elTodoAllBtn.appendChild(elBadgeBtn);
 
-    const elTodoInputValue = elTodoInput.value.trim();
+    const elInputValue = elInput.value.trim();
 
     const newTodo = {
         id: todos[todos.length - 1]?.id + 1 || 0,
-        title: elTodoInputValue,
+        title: elInputValue,
         isCompleted: false,
     }
+
     todos.push(newTodo);
-    
-    renderTodos(todos, elTodoList);    
+    renderSaveTodos();
 
-    elTodoInput.value = null;
+    elInput.value = null;
 })
 
-elTodoAllBtn.addEventListener('click', () => {
-    todos = todos.filter(todo => todo.isCompleted === false || true);
-    elBadgeBtn.textContent = todos.length;
-    elTodoAllBtn.appendChild(elBadgeBtn);
-    renderTodos(todos, elTodoList);
-})
+elBtnGroup.addEventListener('click', (evt) => {
+    const isAllBtn = evt.target.matches('.todo-all__btn') || evt.target.matches('.todo-all__badge');
+    const isCompletedBtn = evt.target.matches('.todo-completed__btn') || evt.target.matches('.todo-completed__badge');
+    const isUncompletedBtn = evt.target.matches('.todo-uncompleted__btn') || evt.target.matches('.todo-uncompleted__badge');
 
-elTodoCompBtn.addEventListener('click', () => {
-    const todoTrue = todos.filter(todo => todo.isCompleted === true);
-    elBadgeBtn.textContent = todoTrue.length;
-    elTodoCompBtn.appendChild(elBadgeBtn);
-    renderTodos(todoTrue, elTodoList);
-})
-
-elTodoUnCompBtn.addEventListener('click', () => {
-    const todoFalse = todos.filter(todo => todo.isCompleted === false);
-    elBadgeBtn.textContent = todoFalse.length;
-    elTodoUnCompBtn.appendChild(elBadgeBtn);
-    renderTodos(todoFalse, elTodoList);
+    if (isAllBtn) {
+        renderTodos(todos, elTodoList);
+    }
+    else if (isCompletedBtn) {
+        const filterCompleted = todos.filter(todo => todo.isCompleted === true);
+        renderTodos(filterCompleted, elTodoList);
+    }
+    else if (isUncompletedBtn) {
+        const filterUncompleted = todos.filter(todo => todo.isCompleted === false);
+        renderTodos(filterUncompleted, elTodoList);
+    }
 })
